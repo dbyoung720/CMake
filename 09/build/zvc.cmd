5cdf4c455736e90e6877ce4e6910943926e1f56e
dbyoung720 <dbyoung@sina.com>
2022-07-25 16:19:10 +0800

    update

--- 09/build/zvc.cmd
+++ 09/build/zvc.cmd
@@ -1,7 +1,18 @@
 @echo off
+Color A
 
-cmake -G "Visual Studio 16 2019" -Thost=x86  -A Win32 ..
-pause
+:: …Ë÷√ VS ±‡“Îª∑æ≥
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\vsdevcmd.bat" -no_logo -arch=x86
+
+cmake -G "Visual Studio 17 2022" -Thost=x86  -A Win32 ..
 
 cmake .
+
+MSBuild.exe sqlite3.sln /nologo /consoleloggerparameters:Verbosity=minimal /maxcpucount /nodeReuse:true^
+  /target:Build /property:Configuration=Release;Platform=Win32^
+  /flp1:LogFile=zxerr.log;Verbosity=diagnostic;errorsonly^
+  /flp2:LogFile=zxwas.log;Verbosity=diagnostic;warningsonly
+
 cmake --build . --config Release --target install
+
+pause
